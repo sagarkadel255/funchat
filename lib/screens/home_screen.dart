4da254ee2d327de5f55351e815115838cf0a1526
Sagar Kadel <sagarkadel97@gmail.com>
2025-12-19 18:22:02 +0545

    Connect home screen to dashboard and update color constants

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("FunChat Dashboard"), backgroundColor: Colors.redAccent),
-      body: const Center(
-        child: Text(
-          "Welcome to Dashboard!",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+    return const DashboardScreen();
+      
+    
   }
 }
